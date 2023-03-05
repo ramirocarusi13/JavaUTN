a1c0f8d13908f560f5e0e876e1a11ce23ed0d9e6
ramirocarusi13 <84150063+ramirocarusi13@users.noreply.github.com>
2023-03-05 05:52:38 -0300

    clase 3
    tp3

---
+++ JavaUTN/src/Main.java
@@ -0,0 +1,61 @@
+public class Main {
+    public static void main(String[] args) {
+        /**
+         *   String palabra = "Hello World";
+         *         char letter = 'l';
+         *         int count = 0;
+         *         for (int i = 0; i < palabra.length(); i++) {
+         *             if (palabra.charAt(i) == letter) {
+         *                 count++;
+         *             }
+         *         }
+         *         System.out.println("La letra '" + letter + "' aparece " + count + " veces en el String '" + palabra + "'");
+         *
+         * B)
+         *         int[] numeros = {5, 2, 8};
+         *         boolean ordenAscendente = false;
+         *
+         *         int[] numerosOrdenados = new int[3];
+         *
+         *         for (int i = 0; i < numeros.length - 1; i++) {
+         *             for (int j = i + 1; j < numeros.length; j++) {
+         *                 // Compara los valores de los números y los intercambia si es necesario
+         *                 if ((ordenAscendente && numeros[i] > numeros[j]) ||
+         *                         (!ordenAscendente && numeros[i] < numeros[j])) {
+         *                     int num = numeros[i];
+         *                     numeros[i] = numeros[j];
+         *                     numeros[j] = num;
+         *                 }
+         *             }
+         *         }
+         *
+         *         // Copia los números ordenados al vector de salida
+         *         for (int i = 0; i < numeros.length; i++) {
+         *             numerosOrdenados[i] = numeros[i];
+         *         }
+         *
+         *         System.out.print("Números ordenados: ");
+         *         for (int i = 0; i < numerosOrdenados.length; i++) {
+         *             System.out.print(numerosOrdenados[i] + " ");
+         *         }
+         *
+         *         C)
+         *         int[] numeros = {5, 10, 15, 60, 50, 50, 40};
+         *
+         *         int x = 20;
+         *
+         *         int numerosSumados = 0;
+         *         for (int i = 0; i < numeros.length; i++) {
+         *             if (x < numeros[i]) {
+         *                 numerosSumados += numeros[i];
+         *             }
+         *         }
+         *         System.out.println("La suma de los números mayores a x es: " + numerosSumados);
+         */
+
+
+
+
+
+    }
+}
